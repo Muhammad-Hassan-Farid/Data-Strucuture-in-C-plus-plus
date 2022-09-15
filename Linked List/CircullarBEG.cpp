0535434f6097360ff61d731620057d7d85e8a21c
Muhammad-Hassan-Farid <hassan.khan.wazir12@gmail.com>
2022-09-15 09:52:47 +0500

    Code

---
+++ Linked List/CircullarBEG.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+Node *first = NULL;
+Node *last = NULL;
+
+void insert(int x)
+{
+    if (first == NULL && last == NULL)
+    {
+        Node *curr = new Node();
+        curr->data = x;
+        curr->next = curr;
+        first = curr;
+        last = curr;
+    }
+    else
+    {
+        Node *curr = new Node();
+        curr->data = x;
+        curr->next = first;
+        last->next = curr;
+        first = curr;
+    }
+}
+void display()
+{
+    Node *curr = first;
+    do
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    } while (curr != first);
+}
+
+int main()
+{
+    insert(1);
+    insert(2);
+    insert(3);
+    insert(4);
+    insert(5);
+    display();
+}
