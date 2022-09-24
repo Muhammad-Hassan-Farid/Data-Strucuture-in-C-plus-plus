3ed6696c1a1c4955ad225c918fe446ca0bcb15db
Muhammad-Hassan-Farid <hassan.khan.wazir12@gmail.com>
2022-09-24 20:33:37 +0500

    Code

---
+++ Linked List/PrintReverse.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+struct  Node
+{
+    int data;
+    Node *next;
+    Node *prev;
+   
+};
+
+Node *first;
+Node *last;
+
+void insert(int x)
+{
+    if (first == NULL && last == NULL)
+    {
+        Node *curr = new Node();
+        curr->data = x;
+        curr->next = NULL;
+        curr->prev = NULL;
+        first = curr;
+        last = curr;
+    }
+    else
+    {
+        Node *curr = new Node();
+        curr->data = x;
+        curr->next = NULL;
+        curr->prev = last;
+        last->next = curr;
+        last = curr;
+    }
+}
+
+void display()
+{
+    Node *curr = last;
+    while (last != first)
+    {
+        cout << curr->data << " ";
+        curr = curr->prev;
+    }
+}
+
+int main()
+{
+insert(1);
+insert(2);
+insert(3);
+insert(4);
+insert(5);
+
+display();
+
+}
