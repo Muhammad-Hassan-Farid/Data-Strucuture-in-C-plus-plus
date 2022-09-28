765f77a4e6680708ed92264ced72d546bfc5e36a
Muhammad-Hassan-Farid <hassan.khan.wazir12@gmail.com>
2022-09-28 17:13:12 +0500

    Code

---
+++ Stack/PushAndPopUsingArray.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int top=-1;
+
+bool IsEmpty()
+{
+    if(top==-1)
+    {
+        return true;
+    }
+    else
+
+    {
+        return false;
+    }
+}
+bool IsFull(int size)
+{
+    if(top==size-1)
+    {
+        cout<<"\nOverFlow.";
+    }
+}
+
+
+void Push(int *arr, int size)
+{
+    
+
+
+
+}
+
+int main()
+{
+    
+}
