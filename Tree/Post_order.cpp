d4379b8dde0a956b3fed537d3a3e03d3f0745934
Muhammad-Hassan-Farid <hassan.khan.wazir12@gmail.com>
2022-11-08 12:22:55 +0500

    Code

---
+++ Tree/Post_order.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data=x;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+void Post_order(struct Node *root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    
+    Post_order(root->left);
+    Post_order(root->right);
+    cout<<root->data<<" ";
+}
+
+
+int main()
+{
+               
+            /*      1
+                  /  \
+                2     3
+               / \   / \
+              4  5  6   7
+            
+            */
+    struct Node *root = new Node(1);
+    root->left= new Node(2);
+    root->right=new Node(3);
+
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+
+    root->right->left=new Node(6);
+    root->right->right=new Node(7);
+
+    cout<<"Pre Order Root Travers: ";
+    Post_order(root);
+    
+    return 0;
+}
